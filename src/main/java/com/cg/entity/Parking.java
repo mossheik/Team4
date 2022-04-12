c2a494ab3dc8508f27412fc3702f457d716762d7
Maurya <aditya-kumar.maurya@capgemini.com>
2022-04-12 15:06:27 +0530

    feat: developed the class for parking

--- src/main/java/com/cg/entity/Parking.java
+++ src/main/java/com/cg/entity/Parking.java
@@ -3,16 +3,12 @@ package com.cg.entity;
 import java.util.HashMap;
 
 public class Parking {
-	
-	public static HashMap<Integer,String> parkingArea;
+
+	public static HashMap<Integer, String> parkingArea;
 
 	public Parking() {
 		super();
-		parkingArea=new HashMap<Integer,String>();
-		parkingArea.put(1, "Vacant");
-		parkingArea.put(2, "Vacant");
-		parkingArea.put(3, "Vacant");
-		
+		parkingArea = new HashMap<Integer, String>();
 	}
 
 	public Parking(HashMap<Integer, String> parkingArea) {
@@ -28,10 +24,55 @@ public class Parking {
 		this.parkingArea = parkingArea;
 	}
 
+	public boolean addParking(int increment, String type) {
+		try {
+			int currentSize = parkingArea.size();
+			int requiredSize = currentSize + increment;
+			for (int i = currentSize; i < requiredSize; ++i) {
+				parkingArea.put(i, type);
+			}
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
+	public boolean removeParking(int decrement) {
+		try {
+			int currentSize = parkingArea.size();
+			for (int i = 0; i < decrement; ++i) {
+				parkingArea.remove(--currentSize);
+			}
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean rangeChangeStatusParking(int start, int end, String status) {
+		try {
+			for (int i = start; i < end; ++i) {
+				parkingArea.replace(i, status);
+			}
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean changeParkingStatus(int position, String status) {
+		try {
+			parkingArea.replace(position, status);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
 	@Override
 	public String toString() {
 		return "Parking [parkingArea=" + parkingArea + "]";
 	}
-	
 
 }
