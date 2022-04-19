709cbeb8fcc37a78a1be4eb514fee6bf5c480f07
adityakr-maurya <102800228+adityakr-maurya@users.noreply.github.com>
2022-04-19 10:50:53 +0530

    feat: added swagger and dependencies

--- src/main/java/com/cg/CarParkingSytstemApplication.java
+++ src/main/java/com/cg/CarParkingSytstemApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableJpaRepositories("com.cg.repository")
 @EntityScan("com.cg.entity")
+@EnableSwagger2
 public class CarParkingSytstemApplication {
 
 	public static void main(String[] args) {
