5944415c3d9ce93c9fe474fd92960048d18bec55
adityakr-maurya <102800228+adityakr-maurya@users.noreply.github.com>
2022-04-19 11:17:20 +0530

    fix: added delete command in sql to support update mode and rerunning

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into person (email, password, role) values ('admin@carparking.com', '12345', 'ADMIN');
+DELETE FROM admin where id=1;
+DELETE FROM PERSON where id=1;
+insert into person (id, email, password, role) values (1, 'admin@carparking.com', '12345', 'ADMIN');
 insert into admin(address, age, firstname, lastname, phonenumber, id) values ('Delhi, India', 30, 'Markus', 'Maverick', '+91 9936123405', 1);
-
-
