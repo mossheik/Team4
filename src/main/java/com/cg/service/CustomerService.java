213ee950812f78ee666d4689eb1458c415f610e3
adityakrmaurya <adityakrmaurya03@gmail.com>
2022-05-10 10:26:39 +0530

    fix: Exception Error

--- src/main/java/com/cg/service/CustomerService.java
+++ src/main/java/com/cg/service/CustomerService.java
@@ -1,6 +1,5 @@
 package com.cg.service;
 
-import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
